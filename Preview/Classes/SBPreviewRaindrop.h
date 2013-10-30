a0df9ee8e3b762f0af59875b437ef0044b2deebf
Howard P. Logsdon <howard@hplogsdon.com>
2013-10-30 13:05:04 -0600

    Add the main SBPreviewRaindrop class

---
+++ Preview/Classes/SBPreviewRaindrop.h
@@ -0,0 +1,13 @@
+//
+//  SBPreviewRaindrop.h
+//  Preview
+//
+//  Created by H.P.Logsdon on 10/30/13.
+//  Copyright (c) 2013 Selective Bytes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBPreviewRaindrop : NSObject
+
+@end
---
+++ Preview/Classes/SBPreviewRaindrop.m
@@ -0,0 +1,13 @@
+//
+//  SBPreviewRaindrop.m
+//  Preview
+//
+//  Created by H.P.Logsdon on 10/30/13.
+//  Copyright (c) 2013 Selective Bytes. All rights reserved.
+//
+
+#import "SBPreviewRaindrop.h"
+
+@implementation SBPreviewRaindrop
+
+@end
