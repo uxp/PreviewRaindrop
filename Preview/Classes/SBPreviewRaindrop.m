aeb8e911ae8815f3be938fab6d8279f817b49269
Howard P. Logsdon <howard@hplogsdon.com>
2013-10-30 13:09:07 -0600

    And finally, basic functionality.

--- Preview/Classes/SBPreviewRaindrop.m
+++ Preview/Classes/SBPreviewRaindrop.m
@@ -7,7 +7,43 @@
 //
 
 #import "SBPreviewRaindrop.h"
+#import "Preview.h"
+
 
 @implementation SBPreviewRaindrop
+
+- (NSString *)pasteboardNameForTriggeredRaindrop
+{
+    PreviewApplication *preview = [SBApplication applicationWithBundleIdentifier:@"com.apple.Preview"];
+    NSArray *windows = nil;
+    if ([preview frontmost]) {
+        windows = [[preview windows] get];
+    }
+
+    if (!windows || [windows count] == 0) {
+        return nil;
+    }
+
+    NSMutableArray *pbWindows = [NSMutableArray array];
+    for (PreviewWindow *window in windows) {
+        if (![window document]) {
+            return nil;
+        }
+
+        NSURL *pathUrl = [NSURL URLWithString:[[window document] path]];
+        NSPasteboardItem *item = [[NSPasteboardItem alloc] init];
+        [item setString:[pathUrl absoluteString] forType:(NSString *)kUTTypeFileURL];
+        [pbWindows addObject:item];
+    }
+
+    if ([pbWindows count] == 0) {
+        return nil;
+    }
+
+    NSPasteboard *pasteboard = [NSPasteboard pasteboardWithUniqueName];
+    [pasteboard writeObjects:pbWindows];
+
+    return pasteboard.name;
+}
 
 @end
